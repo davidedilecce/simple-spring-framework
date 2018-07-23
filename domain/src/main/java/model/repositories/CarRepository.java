b6d2455a0a1ba95f884885d496d813cd7ae24be5
davidedilecce <davidevincenzo.dilecce@gmail.com>
2018-07-23 23:29:34 +0200

    First config Servet

--- domain/src/main/java/model/repositories/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package model.repositories;
-
-import base.BaseRepository;
-import model.Car;
-
-public interface CarRepository extends BaseRepository<Car> {
-}
---
+++ fe/src/main/java/fe/MainController.java
@@ -0,0 +1,15 @@
+package fe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @RequestMapping("")
+    public String index() {
+        return "index";
+    }
+
+}
